2e2ed80e1c0873c4c9c5a4973d448611bb053c31
sejinniemma <jsj0471@naver.com>
2022-11-17 18:37:00 +0900

    refectoring youtube Api

--- src/context/YoutubeApiContext.js
+++ src/context/YoutubeApiContext.js
@@ -1,8 +1,11 @@
 import React, { createContext, useContext } from 'react';
+import FakeYoutubeClient from '../api/FakeYoutubeClient';
 import Youtube from '../api/youtube';
+import YoutubeClient from '../api/youtubeClient';
 
 export const YoutubeApiContext = createContext();
-const youtube = new Youtube();
+const youtubeClient = new YoutubeClient();
+const youtube = new Youtube(youtubeClient);
 
 export function YoutubeProvider({ children }) {
   return (
