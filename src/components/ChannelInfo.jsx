f0f91a6f0de5b2f8091d8129c2b5e0fd499798ca
sejinniemma <jsj0471@naver.com>
2022-11-18 15:27:15 +0900

    make ChammelInfo

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from '@tanstack/react-query';
 
 export default function ChannelInfo({ id, name }) {
+  const youtube = useYoutubeApi();
+  const { data: url } = useQuery(['channel', id], () =>
+    youtube.channelImageURL(id)
+  );
+
   return (
     <div>
-      <h2>{id}</h2>
-      <h2>{name}</h2>
+      {url && <img src={url} alt={name} />}
+      <p>{name}</p>
     </div>
   );
 }
